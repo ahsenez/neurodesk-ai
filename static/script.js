afce93362d2a9cd7b21fefb5e4f733026df8e8a0
ahsen ezgün <ahsen_ezgun@hotmail.com>
2026-07-13 17:21:59 +0300

    Add command retrieval in sendCommand function

--- static/script.js
+++ static/script.js
@@ -2,3 +2,5 @@ async function sendCommand() {
 
     const input = document.getElementById("command-input");
     const chatBox = document.getElementById("chat-box");
+
+    const command = input.value;
